3806b066b004c834a696352c99c095f1c84fae93
Boldizsar Szabo <szboldisz@gmail.com>
2025-02-18 15:36:06 +0100

    Add files via upload

--- war/szamologep.php
+++ war/szamologep.php
@@ -1,13 +1,38 @@
 <?php
 
-if (isset($_GET['num1']) && isset($_GET['num2'])) {
-    if (isset($_GET['operation'])) {
-        
-    }
+$method = $_SERVER['REQUEST_METHOD'];
 
+if ($method === "GET" && isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['operation'])) {
     $num1 = $_GET['num1'];
     $num2 = $_GET['num2'];
-    $sum = intval($num1) + intval($num2);
-    print_r($sum);
+    $operation = $_GET['operation'];
+
+    print_r(calculate($num1, $num2, $operation));
     exit;
+}
+
+if ($method === "POST" && isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operation'])) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operation = $_POST['operation'];
+
+    print_r(calculate($num1, $num2, $operation));
+    exit;
+}
+
+function calculate($num1, $num2, $operation)
+{
+    $operationPairs = [
+        "" => $num1 + $num2,
+        "+" => $num1 + $num2,
+        "-" => $num1 - $num2,
+        "*" => $num1 * $num2,
+        "%" => $num2 == 0 ? "Division by zero" : $num1 / $num2,
+    ];
+
+    if (array_key_exists($operation, $operationPairs)) {
+        return $operationPairs[$operation];
+    } else {
+        return "Invalid operation";
+    }
 }
